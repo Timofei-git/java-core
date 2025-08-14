5bdae733627f1c4514e75a2ad721c857fa7542a7
Timofey Gridin <timasgridin@gmail.com>
2025-08-14 18:02:51 +0300

    MP18

--- src/main/java/ru/mentee/power/variables/VariableExample.java
+++ src/main/java/ru/mentee/power/variables/VariableExample.java
@@ -1,17 +1,12 @@
 package ru.mentee.power.variables;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class VariableExample {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int myAge = 18;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String myName = "Tima";
+
+        System.out.println("My name is " + myName + " and I am " + myAge + " y.o.");
+
     }
 }
