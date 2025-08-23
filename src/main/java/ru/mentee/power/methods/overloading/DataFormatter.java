c0738cc5bd69ea30b59a955599289c943e67d70e
Timofey Gridin <timasgridin@gmail.com>
2025-08-23 21:49:50 +0300

    Fix

--- src/main/java/ru/mentee/power/methods/overloading/DataFormatter.java
+++ src/main/java/ru/mentee/power/methods/overloading/DataFormatter.java
@@ -116,7 +116,8 @@ public class DataFormatter {
         if (date == null || pattern == null) {
             return "";
         }
-        return new SimpleDateFormat(pattern).format(date);
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.ENGLISH);
+        return sdf.format(date).toLowerCase(Locale.ENGLISH);
     }
 
     /**
